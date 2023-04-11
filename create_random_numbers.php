a7629de5336c8eede1685042d5990d279d536776
haseomg <lovethispool@kakao.com>
2023-04-11 16:09:44 +0000

    random numbers make ok

--- create_random_numbers.php
+++ create_random_numbers.php
@@ -1,8 +1,23 @@
 <?php
 
-$temp = range(1, 8)
+// 배열의 크기가 적을 때 유용
+// $temp = range(1, 8);
+// shuffle($temp);
 
-$randomNum = mt_rand(1, 8);
+// $data = array_slice($temp, 0, 8);
+// print_r($data);
 
-echo $randomNum;
+
+// 범위가 클 때 랜덤 함수 사용
+$count = 0;
+$check = $data2 = array();
+while ($count < 8) {
+    $randomNum = mt_rand(1, 8);
+    if (!isset($check[$randomNum])) {
+        $check[$randomNum] = 1;
+        $data2[] = $randomNum;
+        $count ++;
+    }
+}
+print_r($data2);
 ?>
