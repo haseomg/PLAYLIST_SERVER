71634ebc00e581446e911065868ce7156a8513b1
haseomg <lovethispool@kakao.com>
2023-04-12 12:55:31 +0000

    select songInfo fit the extracted number

--- create_random_numbers.php
+++ create_random_numbers.php
@@ -1,23 +1,39 @@
 <?php
+include "dbcon.php";
 
 // 배열의 크기가 적을 때 유용
-// $temp = range(1, 8);
-// shuffle($temp);
+$temp = range(1, 8);
+shuffle($temp);
 
-// $data = array_slice($temp, 0, 8);
-// print_r($data);
+$data = array_slice($temp, 0, 8);
+$numbers_toString = implode('', $data);
+// echo $numbers_toString;
 
-
-// 범위가 클 때 랜덤 함수 사용
-$count = 0;
-$check = $data2 = array();
-while ($count < 8) {
-    $randomNum = mt_rand(1, 8);
-    if (!isset($check[$randomNum])) {
-        $check[$randomNum] = 1;
-        $data2[] = $randomNum;
-        $count ++;
+// 숫자 하나씩 조회해서 music 테이블에서 정보 가져오기
+for ($i = 0; $i < strlen($numbers_toString); $i++) {
+    $num = substr($numbers_toString, $i, 1);
+    $sql = "SELECT * FROM music WHERE num = '$num'";
+    $result = $conn -> query($sql);
+    if ($result -> num_rows > 0) {
+        while ($row = $result -> fetch_assoc()) {
+            echo "artist : " . $row["artist"] . "_____name : " . $row["name"] . "_____time : " . $row["time"] . "<br>";
+        }
+    } else {
+        echo "No results found for number : " . $num . "<br>";
     }
 }
-print_r($data2);
+
+$conn -> close();
+// 범위가 클 때 랜덤 함수 사용
+// $count = 0;
+// $check = $data2 = array();
+// while ($count < 8) {
+//     $randomNum = mt_rand(1, 8);
+//     if (!isset($check[$randomNum])) {
+//         $check[$randomNum] = 1;
+//         $data2[] = $randomNum;
+//         $count ++;
+//     }
+// }
+// print_r($data2);
 ?>
