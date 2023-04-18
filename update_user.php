6dcb6460351bb381d58a5b2bd957c1e45723400b
haseomg <lovethispool@kakao.com>
2023-04-18 15:06:38 +0000

    try update user table from client

--- update_user.php
+++ update_user.php
@@ -2,14 +2,14 @@
 include "dbcon.php";
 
 // POST 방식으로 전달된 파라미터 읽엉
-$num = $_POST['num'];
+$id = $_POST['id'];
 $nickname = $_POST['nickname'];
 
-$sql = "UPDATE user SET nickname = '$nickname' WHERE num = $num";
+$sql = "UPDATE user SET nickname = '$nickname' WHERE id = $id";
 $result = mysqli_query($conn, $sql);
 
 if ($result) {
-    echo 'success';
+    echo $id, $nickname;
 } else {
     echo 'failed';
 }
