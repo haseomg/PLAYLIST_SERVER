2ad80201eb2849e24b3422897172d601120a23a4
haseomg <lovethispool@kakao.com>
2023-04-19 23:28:39 +0000

    music files add (test)

--- create_random_numbers.php
+++ create_random_numbers.php
@@ -7,23 +7,27 @@ shuffle($temp);
 
 $data = array_slice($temp, 0, 8);
 $numbers_toString = implode('', $data);
+// echo "〰️ Random Numbers : ";
+echo $numbers_toString;
+// echo "<br>";
+// echo "<br>";
 // echo $numbers_toString;
 
 // 숫자 하나씩 조회해서 music 테이블에서 정보 가져오기
-for ($i = 0; $i < strlen($numbers_toString); $i++) {
-    $num = substr($numbers_toString, $i, 1);
-    $sql = "SELECT * FROM music WHERE num = '$num'";
-    $result = $conn -> query($sql);
-    if ($result -> num_rows > 0) {
-        while ($row = $result -> fetch_assoc()) {
-            echo "artist : " . $row["artist"] . "_____name : " . $row["name"] . "_____time : " . $row["time"] . "<br>";
-        }
-    } else {
-        echo "No results found for number : " . $num . "<br>";
-    }
-}
+// for ($i = 0; $i < strlen($numbers_toString); $i++) {
+//     $num = substr($numbers_toString, $i, 1);
+//     $sql = "SELECT * FROM music WHERE num = '$num'";
+//     $result = $conn -> query($sql);
+//     if ($result -> num_rows > 0) {
+//         while ($row = $result -> fetch_assoc()) {
+//             echo "🎤artist : " . $row["artist"] . " ⎻⎻⎻⎻⎻ 🎵name : " . $row["name"] . " ⎻⎻⎻⎻⎻ ⏳time : " . $row["time"] . "<br>";
+//         }
+//     } else {
+//         echo "No results found for number : " . $num . "<br>";
+//     }
+// }
 
-$conn -> close();
+// $conn -> close();
 // 범위가 클 때 랜덤 함수 사용
 // $count = 0;
 // $check = $data2 = array();
